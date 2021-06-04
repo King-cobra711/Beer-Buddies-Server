184f4bbb19cd08a6b453870ed2a75be8e2e759d6
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-04 17:16:06 +1000

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./index.js");
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   // console.log(`running on port ${PORT}`);
   console.log(`running on port 3001`);
 });
